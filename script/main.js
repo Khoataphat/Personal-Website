6db44ce3f441402d4c652f968573a1d98a1d3c53
Khoa Tap Hat <khoai50km@gmail.com>
2025-10-12 19:08:42 +0700

    Thêm hiệu ứng typing ở trang index

--- script/main.js
+++ script/main.js
@@ -1,1 +1,14 @@
-const swup = new Swup();
+// 1. Khởi tạo Swup cho chuyển trang mượt mà
+document.addEventListener('DOMContentLoaded', function() {
+
+    const swup = new Swup();
+
+    // 2. Khởi tạo Typed.js
+
+    var typed = new Typed('.auto-type', {
+        strings: ['Dang Khoa'],
+        typeSpeed: 50,
+        backSpeed: 50,
+        loop: true
+    });
+});
